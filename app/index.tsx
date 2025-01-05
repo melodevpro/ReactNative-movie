f56adfb9d00898653aa3d25026d73f4a9a9aa411
melomatthewdev <devscustomers@outlook.com>
2025-01-04 21:06:35 -0500

    config project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-blue-400 ">
+      <Text className=" text-white">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
