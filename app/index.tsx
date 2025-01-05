b96ab25a8079626ca64ebae048de044cb09b9eaf
melomatthewdev <devscustomers@outlook.com>
2025-01-04 22:08:30 -0500

    fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
-
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="bg-blue-400 ">
-      <Text className=" text-white">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-black ">
+      <Text className="text-orange-300">app/index.tsx</Text>
+
+      <Link className="text-white" href="/signin">Sign In</Link>
+      <Link className="text-green-600" href="/explore">Explore</Link>
+      <Link className="text-blue-400" href="/profile">Profile</Link>
+      <Link className="text-red-500" href="/properties/1">Property</Link>
     </View>
   );
 }
+
