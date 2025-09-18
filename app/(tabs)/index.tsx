b6baee5dbc1456048a755aa4d1854c3f9a0f30b0
melomatthewdev <devscustomers@outlook.com>
2025-09-18 11:39:26 -0500

    menu barra multiplataforma

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-200">
+    <View className="flex-1 items-center justify-center ">
       <Text className="font-bold my-10 font-rubik text-5xl text-dark-200">Bienvenido!</Text>
       
     </View>
