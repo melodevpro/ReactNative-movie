da7dfeafabc5d4fa5f91b1050d38b3ffba1cbecc
melomatthewdev <devscustomers@outlook.com>
2025-09-18 20:05:06 -0500

    movieCard

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import icons from '@/assets/constants/icons'
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date} : Movie) => {
   return (
@@ -16,6 +17,22 @@ const MovieCard = ({ id, poster_path, title, vote_average, release_date} : Movie
                 <Text className='text-sm font-bold text-white mt-2 '>
                     {title}
                 </Text>
+
+                <View className='flex-row items-center justify-start gap-x-1'>
+                <Image source={icons.star} className='size-4'>
+                </Image>
+
+                <Text className='text-xs text-white font-bold uppercase'>{Math.round(vote_average / 2)}</Text>
+                </View>
+
+                <View className='flex-row items-center justify-between'>
+                  <Text className='text-sx text-light-300 font-medium mt-1'>
+                    {release_date?.split('-')[0]}
+                  </Text>
+                  <Text className='text-xs font-medium text-light-300 uppercase'>
+                    Movie
+                  </Text>
+                </View>
         </TouchableOpacity>
     </Link>
   )
